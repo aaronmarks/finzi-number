3e08c721a1434e1dfa927cc1c763a57f60d50803
Aaron Marks <aaron.marks5@gmail.com>
2017-09-01 15:09:46 +0000

    change db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170901125749) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "controllers", force: :cascade do |t|
     t.string   "WeeklyStat"
     t.datetime "created_at", null: false
